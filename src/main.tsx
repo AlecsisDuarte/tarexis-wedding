8a19d5b944ee3fdad95746b0658519d0a8e16976
slimtimtamTara <taralaughlin769@gmail.com>
2025-04-05 21:59:05 -0700

    updated styling

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './main.css'
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <div>
+        <div>
+            Save the Weekend
+        </div>
+        <div>
+            September 4-6, 2026
+        </div>
+        <div>
+            Monte Cova, Ensenada, B.C Mexíco
+        </div>
+    </div>
+  )
