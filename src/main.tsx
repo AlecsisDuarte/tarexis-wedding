97e68cc6a855ce6254b439e6af5797714454b6af
slimtimtamTara <taralaughlin769@gmail.com>
2025-04-15 14:50:14 -0700

    attempting to set up website base

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-    <BrowserRouter basename={"/"}>
+    <BrowserRouter basename={"/tarexis-wedding/"}>
     <div className="std-main">
         <img src={std}></img>
     </div>
