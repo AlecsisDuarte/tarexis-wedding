0ae11ffadb66d9870b435c6f2c38be8e525247cf
alexisduarte <alexismiguelduarte@gmail.com>
2025-05-22 22:41:25 -0700

    More formatting

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
-import Home from './pages/home'
-import Travel from './pages/travel'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./pages/home";
+import Travel from "./pages/travel";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <BrowserRouter basename={"/tarexis-wedding/"}>
-    <Routes>
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/travel" element={<Travel />} />
-    </Routes>
+      </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
-  )
+  </React.StrictMode>
+);
