5fd847fa5b930dd433624a9ae3a625885530adfd
Alexis Duarte <alexismiguelduarte@gmail.com>
2025-10-12 23:57:14 -0700

    Improved the travel page

--- src/pages/Travel.tsx
+++ src/pages/Travel.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Travel.css";
 import { useLanguage } from "../LanguageContext";
+import elCielo from "../assets/el_cielo.jpg";
+import carreteraTijuanaEnsenada from "../assets/carretera_tijuana_ensenada.jpg";
+import monteCova from "../assets/monte_cova.jpg";
 
 const Travel: React.FC = () => {
   const { t } = useLanguage();
@@ -10,55 +13,113 @@ const Travel: React.FC = () => {
     <div className="travel-container">
       <h1 className="pages-titles">{t("travel")}</h1>
 
-      <section className="travel-section">
-        <h2 className="travel-section-title">{t("whereToStay")}</h2>
-        <p>{t("whereToStayIntro")}</p>
-        <p>{t("whereToStayHubs")}</p>
-        <h3 className="hotel-options">{t("hotelOptions")}</h3>
-        <div className="hotel-list">
-          <div className="hotel">
-            <h4>{t("hotelAguaDeVid")}</h4>
-            <p>{t("hotelAguaDeVidDesc")}</p>
+      <section className="travel-section travel-section-left">
+        <img
+          src={elCielo}
+          alt="El Cielo"
+          className="travel-section-image where-to-stay-image"
+        />
+        <div className="travel-section-content where-to-stay-content">
+          <h2 className="travel-section-title">{t("whereToStay")}</h2>
+          <p>{t("whereToStayIntro")}</p>
+          <div className="hotel-list">
+            <div className="hotel">
+              <a
+                href="https://elcielovalledeguadalupe.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h4 className="hotel-name">{t("hotelElCieloName")}</h4>
+              </a>
+              <p>{t("hotelElCieloDesc")}</p>
+            </div>
+            <div className="hotel">
+              <a
+                href="https://hotelandana.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h4 className="hotel-name">{t("hotelAndanaName")}</h4>
+              </a>
+              <p>{t("hotelAndanaDesc")}</p>
+            </div>
+            <div className="hotel">
+              <a
+                href="https://hotellosamantesvalledeguadalupe.mx/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h4 className="hotel-name">{t("hotelLosAmantesName")}</h4>
+              </a>
+              <p>{t("hotelLosAmantesDesc")}</p>
+            </div>
+            <div className="hotel">
+              <a
+                href="https://www.hotelentrevalle.com/en"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <h4 className="hotel-name">{t("hotelEntrevalleName")}</h4>
+              </a>
+              <p>{t("hotelEntrevalleDesc")}</p>
+            </div>
           </div>
-          <div className="hotel">
-            <h4>{t("hotelElCielo")}</h4>
-            <p>{t("hotelElCieloDesc")}</p>
-          </div>
-          <div className="hotel">
-            <h4>{t("hotelEncuentroGuadalupe")}</h4>
-            <p>{t("hotelEncuentroGuadalupeDesc")}</p>
-          </div>
-          <div className="hotel">
-            <h4>{t("hotelBruma")}</h4>
-            <p>{t("hotelBrumaDesc")}</p>
-          </div>
-          <div className="hotel">
-            <h4>{t("airbnb")}</h4>
-            <p>{t("airbnbDesc")}</p>
+          <p>{t("whereToStayOutro")}</p>
+        </div>
+      </section>
+
+      <section className="travel-section travel-section-right">
+        <img
+          src={carreteraTijuanaEnsenada}
+          alt="Carretera Tijuana Ensenada"
+          className="travel-section-image getting-in-image"
+        />
+<div className="travel-section-content getting-in-content">
+          <h2 className="travel-section-title">{t("gettingIn")}</h2>
+          <p>{t("gettingInIntro_p1")}</p>
+          <p>{t("gettingInIntro_p2")}</p>
+          <p>
+            <strong>{t("gettingInIntro_remindersTitle")}</strong>
+          </p>
+          <ul>
+            <li>
+              <strong>{t("gettingInIntro_reminders_p1_title")}</strong>
+              {t("gettingInIntro_reminders_p1_text")}
+            </li>
+            <li>
+              <strong>{t("gettingInIntro_reminders_p2_title")}</strong>
+              {t("gettingInIntro_reminders_p2_text")}
+            </li>
+            <li>
+              <strong>{t("gettingInIntro_reminders_p3_title")}</strong>
+              {t("gettingInIntro_reminders_p3_text")}
+            </li>
+          </ul>
+          <p>{t("gettingInIntro_p3")}</p>
+          <div className="getting-in-links">
+            <Link to="/from-usa" className="travel-link">
+              {t("fromUSContent2")}
+            </Link>
+            <Link to="/from-mexico" className="travel-link">
+              {t("fromMXContent")}
+            </Link>
           </div>
         </div>
       </section>
 
-      <section className="travel-section">
-        <h2 className="travel-section-title">{t("gettingIn")}</h2>
-        <p>{t("gettingInIntro")}</p>
-        <div className="getting-in-links">
-          <Link to="/from-usa" className="travel-link">
-            {t("fromUSContent2")}
-          </Link>
-          <Link to="/from-mexico" className="travel-link">
-            {t("fromMXContent")}
-          </Link>
+      <section className="travel-section travel-section-left">
+        <img
+          src={monteCova}
+          alt="Monte Cova"
+          className="travel-section-image"
+        />
+        <div className="travel-section-content">
+          <h2 className="travel-section-title">{t("movingAround")}</h2>
+          <p>{t("gettingAroundTip")}</p>
+          <p>
+            <strong>{t("lomasTravel")}</strong>
+          </p>
         </div>
-      </section>
-
-      <section className="travel-section">
-        <h2 className="travel-section-title">{t("movingAround")}</h2>
-        <p>{t("movingAroundIntro")}</p>
-        <p>{t("gettingAroundTip")}</p>
-        <p>
-          <strong>{t("lomasTravel")}</strong>
-        </p>
       </section>
     </div>
   );
